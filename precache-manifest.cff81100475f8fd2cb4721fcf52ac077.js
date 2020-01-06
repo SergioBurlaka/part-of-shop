683782c1806650103efcbd5058899e596fdae5b3
SergioBurlaka <burlaka01@gmail.com>
2020-01-06 18:55:45 +0200

    Updates

---
+++ precache-manifest.cff81100475f8fd2cb4721fcf52ac077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000979f9c7be68a94a30d1950cbea309",
+    "url": "/SergioBurlaka/part-of-shop.git/index.html"
+  },
+  {
+    "revision": "9967e6baf56e110eae78",
+    "url": "/SergioBurlaka/part-of-shop.git/static/css/main.9746845e.chunk.css"
+  },
+  {
+    "revision": "01f5cf3bbf47bd96ee50",
+    "url": "/SergioBurlaka/part-of-shop.git/static/js/2.db2bfa6d.chunk.js"
+  },
+  {
+    "revision": "9967e6baf56e110eae78",
+    "url": "/SergioBurlaka/part-of-shop.git/static/js/main.03f857b6.chunk.js"
+  },
+  {
+    "revision": "e0c475423949ba041761",
+    "url": "/SergioBurlaka/part-of-shop.git/static/js/runtime~main.a2bcf477.js"
+  }
+]);
